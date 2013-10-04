680a89e7aa36c652fe25131903d21bd1be3741f5
Ryan Ramage <ryan.ramage@gmail.com>
2013-10-04 13:36:19 -0600

    Inital commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs'),
+    chokidar = require('chokidar'),
+    paste = require('copy-paste'),
+    nodecr = require('nodecr'),
+    suffix = '.png',
+    path = getUserHome() + '/Desktop',
+    watcher = chokidar.watch(path, {ignored: /\/\./, persistent: true});
+
+watcher
+  .on('add', check);
+
+function check(file) {
+    if (!valid(file)) return;
+
+
+    // move the file to a temp place
+    var temp = path + '/' + new Date().getTime() + '.png';
+    fs.renameSync(file, temp);
+    nodecr.process(temp,function(err, text) {
+        if(err) return console.log(err);
+        fs.unlink(temp, function(err){});
+        copy(text);
+    });
+}
+
+
+function getUserHome() {
+  return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
+
+function valid(file) {
+    return file.indexOf(suffix, file.length - suffix.length) !== -1;
+}
